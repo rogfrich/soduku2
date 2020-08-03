fa80fd6a93d5774d815ba9a458cf129469d773c3
richard.a.cairns@gmail.com <thehobbyistprogrammer@gmail.com>
2020-08-03 17:41:13 +0100

    decouple tests from main program, allow them to create their own instance of the grid. Refactor test data into a dict

--- test_sudoku.py
+++ test_sudoku.py
@@ -4,6 +4,14 @@ import main, config
 d = main.load_data_from_config()
 g = main.create_grid(d)
 
+test_data = {
+    'valid_data': '200910568000254100100000300300009600958062004074038010081040026000700800006891003',
+    'bad_data_invalid_sudoku': '222910568000254100100000300302222220958062004074038010081040026000700800006891003',
+    'bad_data_short': '200910500254100100000300300009600958062004074038010081040026000700800006891003',
+    'bad_data_invalid_character': '2X0910568000254100100000300300009600958062004074038010081040026000700800006891003',
+    'qqwing_format_data': '....1...5....93.48.2.4.5........1.6..3...4.2....9....4...1.........56.9356.2..41.',
+}
+
 
 def test_create_grid():
     """
@@ -81,21 +89,27 @@ def test_get_impossibles():
     """
     Test that get_impossibles() correctly returns the impossible values for a cell.
     """
-    assert main.get_impossibles("01") == set(["1", "2", "5", "6", "7", "8", "9"])
+    d = config.test_data['valid_data']
+    g = main.create_grid(d)
+    assert main.get_impossibles("01", test_grid=g) == {"1", "2", "5", "6", "7", "8", "9"}
 
 
 def test_get_possibles():
     """
     Test that get_possibles() correctly returns the possible values for a cell.
     """
-    assert main.get_possibles("01") == set(["3", "4"])
+    d = config.test_data['valid_data']
+    g = main.create_grid(d)
+    assert main.get_possibles("01", test_grid=g) == {"3", "4"}
 
 
 def test_get_solved_cells():
     """
     Test that get_solved_cells returns the correct number for the test data, and that cell 00 (solved) is returned and cell 01 (not solved) isn't.
     """
-    sc = main.get_solved_cells()
+    d = config.test_data['valid_data']
+    g = main.create_grid(d)
+    sc = main.get_solved_cells(test_grid=g)
     assert len(sc) == 38 and "00" in sc and not "01" in sc
 
 
@@ -114,7 +128,7 @@ def test_validate_data_short():
     Test that validate_data() raises an exception if the supplied data is not 81 chars long.
     """
     with pytest.raises(AssertionError):
-        d = config.bad_data_short
+        d = config.test_data['bad_data_short']
         main.validate_data(d)
 
 
@@ -123,7 +137,7 @@ def test_validate_data_non_numeric():
     Test that  validate_data() raises an exception if a non-numeric char is present in the data.
     """
     with pytest.raises(AssertionError):
-        d = config.bad_data_invalid_character
+        d = config.test_data['bad_data_invalid_character']
         main.validate_data(d)
 
 
@@ -131,14 +145,15 @@ def test_validate_data_valid():
     """
     Test that validate_data() does not raise an exception if the data is valid.
     """
-    d = config.valid_data
+    d = config.test_data['valid_data']
     main.validate_data(d)
+
 
 def test_convert_qqwing_data_to_standard_format():
     """
     Test that convert_qqwing_to_standard_format() returns a correctly formatted string.
     """
-    d = config.qqwing_format_data
+    d = config.test_data['qqwing_format_data']
     converted_data = main.convert_qqwing_to_standard_format(d)
     assert len(converted_data) == 81
     assert '.' not in converted_data
